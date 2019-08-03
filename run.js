d5881bc8663b3549d7959e31ae6651dd9bfea7f5
unknown <inusha@outlook.com@>
2019-08-03 18:51:34 +0530

    added anti anti cheat precautions

--- run.js
+++ run.js
@@ -1,1 +1,5 @@
-chrome.webRequest.onBeforeRequest.addListener(function(details) { return {redirectUrl:chrome.extension.getURL("game.js")}; }, { urls: ["https://krunker.io/js/game*.js*"] }, ["blocking"]);
+URLS = ["https://krunker.io/js/game*", "https://krunker.io/libs/zip-ext.*", "https://krunker.io/libs/zip.*"];
+chrome.webRequest.onBeforeRequest.addListener(function(details) { return {redirectUrl:chrome.extension.getURL("game.js")}; }, { urls: [URLS[0]] }, ["blocking"]);
+chrome.webRequest.onBeforeRequest.addListener(function(details) { return {redirectUrl:chrome.extension.getURL("zip-ext.js")}; }, { urls: [URLS[1]] }, ["blocking"]);
+chrome.webRequest.onBeforeRequest.addListener(function(details) { return {redirectUrl:chrome.extension.getURL("zip.js")}; }, { urls: [URLS[2]] }, ["blocking"]);
+chrome.webRequest.onHeadersReceived.addListener(function(details) { return {cancel: details.type == "script"} }, { urls: ["https://krunker.io/*"] }, ["blocking"]);
