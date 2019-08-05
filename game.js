71f180b36aaee24385ba674887d6442b96ff194e
unknown <inusha@outlook.com@>
2019-08-05 23:27:50 +0530

    fake lag logic

--- game.js
+++ game.js
@@ -66312,14 +66312,14 @@ window.addEventListener("keyup", function(e) {
             for (var w = 0; w < e.players.list.length; ++w) {
                 if (tmpObj = e.players.list[w], !tmpObj.active) continue;
                 if (tmpObj.isYou || !tmpObj.objInstances) continue;
-                if (!s) break;
+                if (!s || !r) break;
                 if (!s.active) break;
                 if (s.team != null && tmpObj.team == s.team && stringToInt[state['ESP'].a[state['ESP'].active]] == 1) continue; // why would we want team mate esp
                 if ((_ = tmpObj.objInstances.position.clone()).y += i.playerHeight + i.nameOffset - tmpObj.crouchVal * i.crouchDst, 0 <= tmpObj.hatIndex && (_.y += i.nameOffsetHat), !(1 <= 20 * (S = Math.max(.3, 1 - r.getDistance3D(b.x, b.y, b.z, _.x, _.y, _.z) / 600)) && n.frustum.containsPoint(_))) continue;
                 var distance = Math.abs(__this.object.rotation.y - __r.getDirection(__this.object.position.z, __this.object.position.x, tmpObj.z, tmpObj.x));
                 var inView = null == e.canSee(s, tmpObj.x2, tmpObj.y2 + stringToInt[state['Target'].a[state['Target'].active]] - tmpObj.crouchVal * i.crouchDst, tmpObj.z2);
 
-                if (inView && !threatFakeLag) {
+                if (inView && !threatFakeLag && (s.team == null || tmpObj.team != s.team)) {
                     var calcAngDistance = function(a, b) {
                         var requiredDireY = __r.getDirection(a.z, a.x, b.z, b.x);
                         var requiredDireX = __r.getXDir(a.x, a.y + i.playerHeight - a.crouchVal * i.crouchDst, a.z, b.x, b.y - 3, b.z);
