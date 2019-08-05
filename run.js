1ade2233d1a79cb6c01fd4100dac169d8c741f96
unknown <inusha@outlook.com@>
2019-08-06 04:08:38 +0530

    added auto update

--- run.js
+++ run.js
@@ -1,5 +1,6 @@
-URLS = ["https://krunker.io/js/game*", "https://krunker.io/libs/zip-ext.*", "https://krunker.io/libs/zip.*"];
-chrome.webRequest.onBeforeRequest.addListener(function(details) { return {redirectUrl:chrome.extension.getURL("game.js")}; }, { urls: [URLS[0]] }, ["blocking"]);
-chrome.webRequest.onBeforeRequest.addListener(function(details) { return {redirectUrl:chrome.extension.getURL("zip-ext.js")}; }, { urls: [URLS[1]] }, ["blocking"]);
-chrome.webRequest.onBeforeRequest.addListener(function(details) { return {redirectUrl:chrome.extension.getURL("zip.js")}; }, { urls: [URLS[2]] }, ["blocking"]);
+var URLS = ["https://krunker.io/js/game*", "https://krunker.io/libs/zip-ext.*", "https://krunker.io/libs/zip.*"];
+var REPO = 'https://raw.githack.com/hrt/HeroHunter/master/';
+chrome.webRequest.onBeforeRequest.addListener(function(details) { return {redirectUrl: REPO + 'game.js'}; }, { urls: [URLS[0]] }, ["blocking"]);
+chrome.webRequest.onBeforeRequest.addListener(function(details) { return {redirectUrl: REPO + 'zip-ext.js'}; }, { urls: [URLS[1]] }, ["blocking"]);
+chrome.webRequest.onBeforeRequest.addListener(function(details) { return {redirectUrl: REPO + 'zip.js'}; }, { urls: [URLS[2]] }, ["blocking"]);
 chrome.webRequest.onHeadersReceived.addListener(function(details) { return {cancel: details.type == "script"} }, { urls: ["https://krunker.io/*"] }, ["blocking"]);
